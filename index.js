097c9f95615f8f0ff1a3db5cb1ae1db6179edb14
Eduardo <eduardocandaten10@gmail.com>
2024-09-15 17:37:26 -0300

    recoded

--- index.js
+++ index.js
@@ -3,70 +3,58 @@ const puppeteer = require('puppeteer');
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 (async () => {
+  const email = 'your email';
+  const password = 'your password';
+
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
   });
   const page = await browser.newPage();
 
+  // Open the page
   await page.goto('https://proof.alboompro.com/');
 
-  async function slowType(selector, text) {
-    for (let i = 0; i < text.length; i++) {
-      await page.type(selector, text[i]);
-      await sleep(200); // Espera 200ms entre cada letra
-    }
-  }
-
   // Email
+  await sleep(1000);
   await page.waitForSelector('input[type="email"]');
-  await slowType('input[type="email"]', 'email');
+  await page.type('input[type="email"]', email, { delay: 75 });
 
-  // Senha
-  await page.waitForSelector('input[type="password"]');
-  await slowType('input[type="password"]', 'senha');
+  // Password
+  await sleep(1000);
+  await page.type('input[type="password"]', password, { delay: 75 });
 
-  // Botão de login
-  await sleep(200);
+  // Login button
+  await sleep(1000);
   await page.click('button[type="submit"]');
 
-  // Primeira galeria
+  // First galery
+  await sleep(1000);
   await page.waitForSelector(
     '.src-components-Dashboard-___Dashboard__content___14VBP > div:nth-child(4) a',
   );
+  await page.click(
+    '.src-components-Dashboard-___Dashboard__content___14VBP > div:nth-child(4) a',
+  );
 
-  while (true) {
-    // Primeira galeria
-    await sleep(200);
-    await page.click(
-      '.src-components-Dashboard-___Dashboard__content___14VBP > div:nth-child(4) a',
-    );
+  // "Action" button
+  await sleep(1000);
+  await page.waitForSelector('.sc-hdPSEv.gLaHWW');
+  await page.click('.sc-hdPSEv.gLaHWW');
 
-    // Botão de Ações
-    await page.waitForSelector('.sc-hdPSEv.gLaHWW');
-    await sleep(200);
-    await page.click('.sc-hdPSEv.gLaHWW');
+  // "Delete" button
+  await sleep(1000);
+  await page.click('.sc-cCbPEh.jmjtag');
 
-    // Botão de Deletar
-    await page.waitForSelector('.sc-cCbPEh.jmjtag');
-    await sleep(200);
-    await page.click('.sc-cCbPEh.jmjtag');
+  // Checked Checkbox
+  await sleep(1000);
+  await page.click('input[type=checkbox]');
 
-    // Marca a Checkbox
-    await page.waitForSelector('input[type=checkbox]');
-    await sleep(200);
-    await page.click('input[type=checkbox]');
+  // "Delete" button
+  await sleep(1000);
+  await page.click(
+    '.src-components-Common-___Common__dialogModalContent___1vtiH button.src-components-Common-___Common__button___1H56_:last-child',
+  );
 
-    // Botão de Deletar
-    await page.waitForSelector(
-      '.src-components-Common-___Common__dialogModalContent___1vtiH button.src-components-Common-___Common__button___1H56_:last-child',
-    );
-    await sleep(200);
-    await page.click(
-      '.src-components-Common-___Common__dialogModalContent___1vtiH button.src-components-Common-___Common__button___1H56_:last-child',
-    );
-
-    // Aguarda 8 segundos antes de repetir o ciclo
-    await sleep(8000);
-  }
+  await sleep(8000);
 })();
